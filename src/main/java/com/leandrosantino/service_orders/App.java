9bd6cb2b532f088cd79df17ec9d96c7ac202434b
leandrosantino <leandrosantino2013@gmail.com>
2024-05-25 22:16:47 -0300

    update

--- src/main/java/com/leandrosantino/service_orders/App.java
+++ src/main/java/com/leandrosantino/service_orders/App.java
@@ -1,11 +1,15 @@
 package com.leandrosantino.service_orders;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.leandrosantino.service_orders.infra.FxApplication;
 
 import javafx.application.Application;
+import net.rgielen.fxweaver.core.FxWeaver;
+import net.rgielen.fxweaver.spring.SpringFxWeaver;
 
 @EnableScheduling
 @SpringBootApplication
@@ -14,5 +18,10 @@ public class App {
 	public static void main(String[] args) {
 		System.setProperty("java.awt.headless", "false");
 		Application.launch(FxApplication.class, args);
+	}
+
+	@Bean
+	FxWeaver fxWeaver(ConfigurableApplicationContext applicationContext) {
+		return new SpringFxWeaver(applicationContext);
 	}
 }
